322b7bd6c9d6bf1e34e8473314ae4ec9e48e2017
daniel <dlsclwnssla@gmail.com>
2024-05-21 16:33:28 +0900

    Circle class

--- Circle.h
+++ Circle.h
@@ -1,11 +1,29 @@
 class Circle
 {
-    private:
-        int radius = 0;
     public:
         Circle();
         ~Circle();
         Circle(int r);
         int getRadius();
         double getArea();
+    private:
+        int radius = 0;
+    protected:
+        void setRadius(int r);
 };
+
+class Ecllipse : public Circle{
+    private:
+        int width = 0;
+    public:
+        Ecllipse(int r, int w){
+            setRadius(r);
+            setWidth(w);
+        };
+        double getArea(){
+            return 3.14 * getRadius() * getRadius();
+        };
+        void setWidth(int w){
+            width = w;
+        }
+}
