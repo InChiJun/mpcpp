b72e69400cc070d42c32bcb41051427e86bf8854
daniel <dlsclwnssla@gmail.com>
2024-05-28 14:25:29 +0900

    git code upload

--- test.cpp
+++ test.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
-#include <cstring>
-#include <cstdlib>
-
 using namespace std;
 
-class Random{
-	public:
-		Random(){}
-		int next(){
-			return rand()%32768;
-		}
-		int nextInRange(int start, int end){
-			int range = end - start + 1;
-			return (rand()%range) + start;
-		}
-};
-
+char& find(char s[], int index){
+	return s[index];
+}
 
 int main(int argc, char const *argv[]){
-	Random r;
-	cout << "-- 0에서 " << 32767 << "까지의 랜덤 정수 10 개--" << endl;
-	for(int i = 0; i < 10; i++){
-		int n = r.next();
-		cout << n << ' ';
-	}
-	cout << endl << endl << "-- 2에서 " << "4 까지의 랜덤 정수 10 개 --" << endl;
-	for(int i = 0; i < 10; i++){
-		int n = r.nextInRange(2, 4);
-		cout << n << ' ';
-	}
-	cout << endl;
+	char name[] = "Mike";
+	cout << name << endl;
+
+	find(name, 0) = 'S';
+	cout << name << endl;
+
+	char ref = find(name, 2);
+	ref = 't';
+
+	cout << name <<endl;
+
     return 0;
 }
